0a6b1a204e1757badc288f866068d2046e1ac6b4
Guilleloper <pgaugusto24@gmail.com>
2025-01-31 22:45:47 -0500

    Agregar archivos de proyecto.

---
+++ CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace CleanArchitecture.Domain.Abstractions
+{
+    public abstract class Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public Guid Id { get; init; }
+
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public IReadOnlyList<IDomainEvent> GetDomainEvents()
+        {
+            return _domainEvents.ToList();
+        } 
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+    }
+}
